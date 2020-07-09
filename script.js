365470800d095fdfcd12a87d7d16f3f4eb9e587a
azeezat123 <sogbesanteniola@gmail.com>
2020-07-09 17:56:52 +0100

    new features

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 
+ function currentSlide(n) {
+   showSlides(slideIndex = n);
+  }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
+ function showSlides(n) {
+   var i;
   var slides = document.getElementsByClassName("container-1");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+       slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+       dots[i].className = dots[i].className.replace(" active", "");
+   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
